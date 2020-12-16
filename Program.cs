b498e58ea021558d7d53560a46f0630733e91f8c
FERNANDA DEL TORO LIRA <I20050585@monclova.tecnm.mx>
2020-12-16 12:18:36 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProyectoConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora Binaria");
+            Console.WriteLine("Agregar botones para una calculadora");
+            Console.WriteLine("Crear los codigos necesarios para numeros binarios");
+            Console.WriteLine("Hacer una operacion para ver su funcionamiento");
+            Console.WriteLine("Dara un resultado en binario");
+            Console.WriteLine("Da un resultado");
+            Console.WriteLine("Comprobar si el resultado es en binario");
+
+
+        }
+    }
+}
